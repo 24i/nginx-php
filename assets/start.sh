58fa0c5b22af6234816d366a691132d795241dd0
Sander Sikkema <sander.sikkema@24i.com>
2017-05-31 20:35:59 +0200

    Rollback execution of killing php-fpm processes

--- assets/start.sh
+++ assets/start.sh
@@ -14,3 +14,14 @@ pid2=$!
 function kill_child_processes {
     kill -$1 $pid1 $pid2
 }
+
+function trap_with_signal() {
+    func="$1" ; shift
+    for sig ; do
+        trap "$func $sig" "$sig"
+    done
+}
+
+trap_with_signal kill_child_processes SIGHUP SIGINT SIGTERM
+
+wait $pid1 $pid2
